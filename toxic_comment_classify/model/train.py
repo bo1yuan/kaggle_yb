22382a076d3a0914d6fc13caf2a3b892e4cbedbd
yuanbo04 <yuanbo04@meituan.com>
2017-12-26 10:55:48 +0800

    add annotation

--- toxic_comment_classify/model/train.py
+++ toxic_comment_classify/model/train.py
@@ -1,8 +1,8 @@
 #encoding=utf8
+import sys
+import os
 import xgboost as xgb
 import numpy as np
 import pandas as pd
-import sys
-import os
 
-#本脚本用来对特征进行抽取，包括一些特征重要性方面的东西
+#本程序进行模型处理
---
+++ toxic_comment_classify/model/train_hp.sh
@@ -0,0 +1,4 @@
+#!bin/sh
+#本脚本用来进行超参数网格搜索
+#需要预定义一些超参数阈值,如hidden_units,filter_num,filter_size等
+echo "aaa"
